f7e6606590423a69a2072b75f368bbed880d52f1
Jess Stewart <jeselistewart@gmail.com>
2017-02-23 09:56:14 -0800

    Update insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,20 +1,18 @@
-def insertion_sort(unsorted_array):
-  sorted_array = [unsorted_array[0]]
-  unsorted_index = 1
-  
-  while unsorted_index < len(unsorted_array):
-    to_add = unsorted_array[unsorted_index]
-    sorted_index = 0
-    while sorted_index < len(sorted_array):
-      print(sorted_index)
-      if sorted_array[sorted_index] < to_add:
-        sorted_index += 1
-      else:
-        print(sorted_index, to_add)
-        sorted_array.append(unsorted_array[unsorted_index])
-        break
-        # sorted_array = sorted_array[:sorted_index] + [unsorted_array[unsorted_index]] + sorted_array[sorted_index:]
-        # sorted_index = len(sorted_array)
-    unsorted_index += 1
-    
-insertion_sort([3, 7, 4, 9, 5, 2, 11])
+def insertion_sort(array):
+	if len(array) < 2: return array
+	key = 1
+
+	while key < len(array):
+		left = key-1
+		# guard for beginning of array, left values should always be less than
+		while left >= 0 and array[key] < array[left]:
+			array[key], array[left] = array[left], array[key]
+			# get back the key
+			key = left
+			left -= 1
+		key += 1	
+	return array	
+
+print(insertion_sort([1, 1, 5, -6]))
+print(insertion_sort([5, 3, 6, -1]))
+print(insertion_sort([8, 9, -70, 9, 3]))
