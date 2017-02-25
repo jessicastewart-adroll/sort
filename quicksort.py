d33de0dfcddc4135f9d55d67997eff736153c25e
Jess Stewart <jeselistewart@gmail.com>
2017-02-25 07:52:45 -0800

    Update quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -18,3 +18,32 @@ def quicksort(array):
   return qless + same + qmore   
   
 print(quicksort([3, 7, 8, 5, 2, 1, 9, 5, 4])) 
+
+### in-place ###
+def partition(array, start, end):
+  pivot = end
+  i = pivot-1
+  while i >= start:
+    j = i
+    while j < pivot and j >= i:
+      if array[pivot] < array[j]:
+        # swap
+        pivot, j = j, pivot
+      j -= 1  
+    i -= 1
+  return pivot
+    
+  
+def quicksort(array, start=0, end=None):
+  if not end:
+    end = len(array)-1
+  if end <= 0: return  
+
+  pivot = partition(array, start, end)
+  print(start, end, array, pivot)
+  # first half
+  quicksort(array, start, pivot-1)
+  # second half
+  quicksort(array, pivot+1, end)
+  
+quicksort([3, 2, 1])  
