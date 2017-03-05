634f3b778086bc18afcac08737b2a740c7946523
Jess Stewart <jeselistewart@gmail.com>
2017-03-05 10:59:32 -0800

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -1,34 +1,14 @@
-def merge(left, right):
-  left_index = 0
-  right_index = 0
-  result = []
+def divide(array, start, end):
+  if end == start:
+    print(array[start])
+    return
   
-  while left_index < len(left) and right_index < len(right):
-    if left[left_index] <= right[right_index]:
-      result.append(left[left_index])
-      left_index += 1
-    else:
-      result.append(right[right_index])
-      right_index += 1
-    
-  # if len(left)-1 > left_index:
-  #   result.extend(left[left_index+1:])
-  # if len(right)-1 > right_index:
-  #   result.extend(right[right_index+1:])  
-  print(result)
-  return result  
-  
-def merge_sort(array):
-  if len(array) == 1:
-    return array
-    
-  mid = len(array)//2  
-  left = array[:mid]
-  right = array[mid:]
-  left = merge_sort(left)
-  right = merge_sort(right)
-  
-  return merge(left, right)
+  mid = end//2 + start
+  divide(array, start, mid)
+  divide(array, mid, end)
 
-test_one = [5, 12, -3, 900]
-merge_sort(test_one)  
+def merge_sort(array):
+  divide(array, 0, len(array)-1)
+    
+  
+merge_sort([-1, -2, -3, -4, -5]) 
