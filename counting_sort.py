a89365062783e1684dd58979ccf0b61896c40ba5
Jess Stewart <jeselistewart@gmail.com>
2017-04-23 13:08:13 -0700

    Update counting_sort.py

--- counting_sort.py
+++ counting_sort.py
@@ -1,3 +1,28 @@
+### with prefix sum
+def counting_sort(array):
+  m = max(array)
+  
+  counts = [0]*(m+1)
+  
+  for i in array:
+    counts[i] += 1
+    
+  i = 1
+  while i < len(counts):
+    counts[i] = counts[i] + counts[i-1]
+    i += 1  
+  
+  i = 0
+  while i < len(counts)-1:
+    if counts[i] != counts[i+1]:
+      array[counts[i]:counts[i+1]] = [i+1]*(counts[i+1]-counts[i])
+    i += 1  
+    
+test = [4, 4, 7, 8, 8, 1]    
+counting_sort(test)
+print(test)
+# [1, 4, 4, 7, 8, 8]
+
 def counting_sort(array):
 	max_value = array[0]
 	for i in array[1:]:
